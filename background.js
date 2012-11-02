3fb2baf9e2899d184bcdead3eb6802a84442322c
Andrew Kane <acekane1@gmail.com>
2012-11-02 15:47:12 -0700

    Initial commit

---
+++ background.js
@@ -0,0 +1,24 @@
+function onClicked(tab) {
+  chrome.tabs.sendMessage(tab.id, {}, function(response) {});
+}
+
+chrome.pageAction.onClicked.addListener(onClicked);
+
+function updatePageAction(tabId) {
+  chrome.tabs.sendRequest(tabId, {is_content_script: true}, function(response) {
+    if (response.is_content_script) {
+      if (response.show) {
+        chrome.pageAction.show(tabId);
+      }
+      else {
+        chrome.pageAction.hide(tabId);
+      }
+    }
+  });
+};
+
+chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
+  if (change.status == "complete") {
+    updatePageAction(tabId);
+  }
+});
---
+++ contentscript.js
@@ -0,0 +1,83 @@
+function untagPhoto() {
+  click(reportLink());
+  waitFor(checkbox, function() {
+    click(checkbox());
+    click(submitButton());
+    waitFor(closeButton, function() {
+      click(closeButton());
+      click(nextLink());
+    });
+  });
+}
+
+// page action conditions
+
+function photoPage() {
+  return window.location.pathname == "/photo.php";
+}
+
+function lightboxActive() {
+  return $("._3qw").length > 0;
+}
+
+function canUntag() {
+  return reportLink().length > 0;
+}
+
+// elements
+
+function reportLink() {
+  return $("a[href^='/ajax/report.php']:visible");
+}
+
+function checkbox() {
+  return $("input[name=untag]:visible");
+}
+
+function submitButton() {
+  return checkbox().closest("form").find("input[type=submit]:visible");
+}
+
+function closeButton() {
+  return $(".uiOverlayButton");
+}
+
+function nextLink() {
+  return $("a.photoPageNextNav");
+}
+
+// helpers
+
+function click(ele) {
+  ele.get(0).click();
+}
+
+function waitFor(eleFunc, callback) {
+  var timer = setInterval( function() {
+    if (eleFunc().length > 0) {
+      clearInterval(timer);
+      callback();
+    }
+  }, 100);
+}
+
+// messaging
+
+function onMessage(request, sender, sendResponse) {
+  untagPhoto();
+  sendResponse({});
+};
+
+// Listen for the content script to send a message to the background page.
+chrome.extension.onMessage.addListener(onMessage);
+
+// http://stackoverflow.com/questions/7325701/chrome-extension-how-to-reload-re-execute-content-script-on-ajax-request
+if (window == top) {
+  chrome.extension.onRequest.addListener( function(req, sender, sendResponse) {
+    var show = false;
+    if (req.is_content_script) {
+      show = photoPage() && !lightboxActive() && canUntag();
+    }
+    sendResponse({is_content_script: true, show: show});
+  });
+};
